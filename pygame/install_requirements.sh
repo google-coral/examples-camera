85a1a58591a7199aba21a56c1cf3afbc871c3c98
Mike Tyka <mtyka@google.com>
2019-07-29 10:31:01 -0700

    Adding and cleaning install_requirements scripts

--- pygame/install_requirements.sh
+++ pygame/install_requirements.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-#
 # Copyright 2019 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://www.apache.org/licenses/LICENSE-2.0
+#    https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,7 +14,10 @@
 # limitations under the License.
 
 if grep -s -q "MX8MQ" /sys/firmware/devicetree/base/model; then
-  echo "OpenCV is not yet suppported for DevBoard"
+  echo "Installing DevBoard specific dependencies"
+  sudo apt-get install -y python3-dev libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev   libsdl1.2-dev libsmpeg-dev python-numpy subversion libportmidi-dev ffmpeg libswscale-dev libavformat-dev libavcodec-dev libfreetype6-dev
+  sudo pip3 install pygame
 else
-  sudo apt install python3-opencv
+  sudo pip3 install pygame
 fi
+
