af0488f367675d472aa96292b21126dd534b353d
octavflorescu <therri1227@gmail.com>
2019-09-13 22:13:00 +0300

    Save extended (15 sec after last face) video to disk. restart countdown timer once a face is re-detected.

--- gstreamer/VideoConverter.py
+++ gstreamer/VideoConverter.py
@@ -7,20 +7,19 @@ import time
 
 
 class VideoConverter:
-    def __init__(self, output_path: str = './videos', fps: float = 24.0, max_video_len_s: int = 40):
+    def __init__(self, output_path: str = './videos', fps: float = 30.0, max_video_len_s: int = 60):
         if not os.path.exists(output_path):
             os.mkdir(output_path)
         self.output_path = output_path
         self.fps = fps
         self.video_writer = None
         self.maximum_video_len_in_seconds = max_video_len_s
-        self.video_creation_timestamp = None
+        self.video_creation_timestamp = 0
         self.video_name = "video.mpg"
 
-    def _init_video_writer_if_needed(self, size:Tuple[int, int] = (320, 240)):
-        if self.video_creation_timestamp is not None and \
-                time.time() - self.video_creation_timestamp > self.maximum_video_len_in_seconds:
-            self.stop_video()
+    def _init_video_writer_if_needed(self, size: Tuple[int, int] = (320, 240)):
+        if time.time() - self.video_creation_timestamp > self.maximum_video_len_in_seconds:
+            self.stop_video_recording()
 
         if self.video_writer is None:
             date_name = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
@@ -34,20 +33,27 @@ class VideoConverter:
 
         return self.video_writer
 
-    def add_image(self, image:np.array):
-        #add condition(s) to start the video
-        self._init_video_writer_if_needed()#image.shape)
+    def start_video_recording(self, image: np.array):
+        video_shape = (image.shape[1], image.shape[0])
+        self._init_video_writer_if_needed(video_shape)
+        self.add_image(image)
+
+    def add_image(self, image: np.array):
         open_cv_image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         self.video_writer.write(open_cv_image)
 
-    def stop_video(self):
+    def stop_video_recording(self):
         if self.video_writer is not None:
             self.video_writer.release()
             print('Closed ' + self.video_name)
             self.video_writer = None
             self.video_name = "video.mpg"
+            self.video_creation_timestamp = 0
 
         cv2.destroyAllWindows()
+
+    def is_video_recording_in_progress(self) -> bool:
+        return self.video_writer is not None
 
 
 
--- gstreamer/classify.py
+++ gstreamer/classify.py
@@ -20,27 +20,47 @@ from edgetpu.classification.engine import ClassificationEngine
 import gstreamer
 import numpy
 import signal
-from VideoConverter import *
-from FaceDetector import *
+
+try:
+    from .VideoConverter import *
+    from .FaceDetector import *
+except Exception: #ImportError
+    from VideoConverter import *
+    from FaceDetector import *
+
+class CONSTANTS:
+    # number of seconds that need to pass after which the video recording is stopped and the video saved to disk
+    NO_FACE_THRESHOLD_S = 15
 
 class Main:
     def __init__(self):
         signal.signal(signal.SIGINT, self.sigint_handler)
         self.video_converter = VideoConverter(output_path='./videos')
         self.face_detector = FaceDetector(model_path='/home/mendel/cameraSamples/examples-camera/all_models/mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite')
+        self.last_face_seen_timestamp = 0
 
     def _callback(self, image, svg_canvas):
         # image.save('out.bmp')
-        if self.face_detector.check_image_contains_face(image):
+        if self.video_converter.is_video_recording_in_progress():
             self.video_converter.add_image(numpy.array(image))
 
+            if self.face_detector.check_image_contains_face(image):
+                self.last_face_seen_timestamp = time.time()
+
+            if time.time() - self.last_face_seen_timestamp >= CONSTANTS.NO_FACE_THRESHOLD_S:
+                self.video_converter.stop_video_recording()
+                self.last_face_seen_timestamp = 0
+
+        elif self.face_detector.check_image_contains_face(image):
+            self.last_face_seen_timestamp = time.time()
+            self.video_converter.start_video_recording(numpy.array(image))
+
     def sigint_handler(self, signum, frame):
-        self.video_converter.stop_video()
+        self.video_converter.stop_video_recording()
 
     def start(self):
         _ = gstreamer.run_pipeline(self._callback, appsink_size=(320, 240))
-        self.video_converter.stop_video()
-
+        self.video_converter.stop_video_recording()
 
 
 def main():
