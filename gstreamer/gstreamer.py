af493454369b5cca233574e0c05f93f6388916d7
Jonas Larsson <ljonas@google.com>
2019-10-04 11:02:54 -0700

    Render to a full screen window when running on Coral dev board

--- gstreamer/gstreamer.py
+++ gstreamer/gstreamer.py
@@ -47,6 +47,8 @@ class GstPipeline:
         bus.add_signal_watch()
         bus.connect('message', self.on_bus_message)
 
+        # Set up a full screen window on Coral, no-op otherwise.
+        self.setup_window()
 
     def run(self):
         # Start inference worker.
@@ -128,6 +130,65 @@ class GstPipeline:
                 if self.overlaysink:
                     self.overlaysink.set_property('svg', svg)
             gstbuffer.unmap(mapinfo)
+
+    def setup_window(self):
+        # Only set up our own window if we have Coral overlay sink in the pipeline.
+        if not self.overlaysink:
+            return
+
+        gi.require_version('GstGL', '1.0')
+        gi.require_version('GstVideo', '1.0')
+        from gi.repository import GstGL, GstVideo
+
+        # Needed to commit the wayland sub-surface.
+        def on_gl_draw(sink, widget):
+            widget.queue_draw()
+
+        # Needed to account for window chrome etc.
+        def on_widget_configure(widget, event, overlaysink):
+            allocation = widget.get_allocation()
+            overlaysink.set_render_rectangle(allocation.x, allocation.y,
+                    allocation.width, allocation.height)
+            return False
+
+        window = Gtk.Window(Gtk.WindowType.TOPLEVEL)
+        window.fullscreen()
+
+        drawing_area = Gtk.DrawingArea()
+        window.add(drawing_area)
+        drawing_area.realize()
+
+        self.overlaysink.connect('drawn', on_gl_draw, drawing_area)
+
+        # Wayland window handle.
+        wl_handle = self.overlaysink.get_wayland_window_handle(drawing_area)
+        self.overlaysink.set_window_handle(wl_handle)
+
+        # Wayland display context wrapped as a GStreamer context.
+        wl_display = self.overlaysink.get_default_wayland_display_context()
+        self.overlaysink.set_context(wl_display)
+
+        drawing_area.connect('configure-event', on_widget_configure, self.overlaysink)
+        window.connect('delete-event', Gtk.main_quit)
+        window.show_all()
+
+        # The appsink pipeline branch must use the same GL display as the screen
+        # rendering so they get the same GL context. This isn't automatically handled
+        # by GStreamer as we're the ones setting an external display handle.
+        def on_bus_message_sync(bus, message, overlaysink):
+            if message.type == Gst.MessageType.NEED_CONTEXT:
+                _, context_type = message.parse_context_type()
+                if context_type == GstGL.GL_DISPLAY_CONTEXT_TYPE:
+                    sinkelement = overlaysink.get_by_interface(GstVideo.VideoOverlay)
+                    gl_context = sinkelement.get_property('context')
+                    if gl_context:
+                        display_context = Gst.Context.new(GstGL.GL_DISPLAY_CONTEXT_TYPE, True)
+                        GstGL.context_set_gl_display(display_context, gl_context.get_display())
+                        message.src.set_context(display_context)
+            return Gst.BusSyncReply.PASS
+
+        bus = self.pipeline.get_bus()
+        bus.set_sync_handler(on_bus_message_sync, self.overlaysink)
 
 def detectCoralDevBoard():
   try:
