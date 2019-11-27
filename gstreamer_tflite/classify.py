9a72f099f44241dadb834a3ce899e62791e225d5
manderfeld <manderfeld@google.com>
2019-11-27 14:35:14 -0800

    Updating gstreamer_tflite comments and docs

--- gstreamer_tflite/classify.py
+++ gstreamer_tflite/classify.py
@@ -12,15 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""A demo which runs object classification on camera frames."""
+"""A demo which runs object classification on camera frames using GStreamer."""
 import argparse
-import time
+import collections
+import gstreamer
+import numpy as np
+import operator
+import os
+from PIL import Image
 import re
 import svgwrite
-import imp
-import os
-from edgetpu.classification.engine import ClassificationEngine
-import gstreamer
+import tflite_runtime.interpreter as tflite
+import time
+
+Class = collections.namedtuple('Class', ['id', 'score'])
+EDGETPU_SHARED_LIB = 'libedgetpu.so.1'
 
 def load_labels(path):
     p = re.compile(r'\s*(\d+)(.+)')
@@ -32,6 +38,51 @@ def generate_svg(dwg, text_lines):
     for y, line in enumerate(text_lines):
       dwg.add(dwg.text(line, insert=(11, y*20+1), fill='black', font_size='20'))
       dwg.add(dwg.text(line, insert=(10, y*20), fill='white', font_size='20'))
+
+def make_interpreter(model_file):
+    model_file, *device = model_file.split('@')
+    return tflite.Interpreter(
+      model_path=model_file,
+      experimental_delegates=[
+          tflite.load_delegate(EDGETPU_SHARED_LIB,
+                               {'device': device[0]} if device else {})
+      ])
+
+def input_size(interpreter):
+    """Returns input image size as (width, height) tuple."""
+    _, height, width, _ = interpreter.get_input_details()[0]['shape']
+    return width, height
+
+def input_tensor(interpreter):
+    """Returns input tensor view as numpy array of shape (height, width, 3)."""
+    tensor_index = interpreter.get_input_details()[0]['index']
+    return interpreter.tensor(tensor_index)()[0]
+
+def output_tensor(interpreter):
+    """Returns dequantized output tensor."""
+    output_details = interpreter.get_output_details()[0]
+    output_data = np.squeeze(interpreter.tensor(output_details['index'])())
+    scale, zero_point = output_details['quantization']
+    return scale * (output_data - zero_point)
+
+def set_input(interpreter, data):
+    """Copies data to input tensor."""
+    input_tensor(interpreter)[:, :] = data
+
+def set_interpreter(interpreter, image, resample=Image.NEAREST):
+    image = image.resize((input_size(interpreter)), resample)
+    set_input(interpreter, image)
+    interpreter.invoke()
+
+def get_output(interpreter, top_k, score_threshold):
+    """Returns no more than top_k classes with score >= score_threshold."""
+    scores = output_tensor(interpreter)
+    classes = [
+        Class(i, scores[i])
+        for i in np.argpartition(scores, -top_k)[-top_k:]
+        if scores[i] >= score_threshold
+    ]
+    return sorted(classes, key=operator.itemgetter(1), reverse=True)
 
 def main():
     default_model_dir = "../all_models"
@@ -49,21 +100,24 @@ def main():
     args = parser.parse_args()
 
     print("Loading %s with %s labels."%(args.model, args.labels))
-    engine = ClassificationEngine(args.model)
+    interpreter = make_interpreter(args.model)
+    interpreter.allocate_tensors()
     labels = load_labels(args.labels)
 
     last_time = time.monotonic()
     def user_callback(image, svg_canvas):
       nonlocal last_time
       start_time = time.monotonic()
-      results = engine.ClassifyWithImage(image, threshold=args.threshold, top_k=args.top_k)
+      set_interpreter(interpreter, image)
+      results = get_output(interpreter, args.top_k, args.threshold)
       end_time = time.monotonic()
       text_lines = [
+          ' ',
           'Inference: %.2f ms' %((end_time - start_time) * 1000),
           'FPS: %.2f fps' %(1.0/(end_time - last_time)),
       ]
-      for index, score in results:
-        text_lines.append('score=%.2f: %s' % (score, labels[index]))
+      for result in results:
+          text_lines.append('score=%.2f: %s' % (result.score, labels.get(result.id, result.id)))
       print(' '.join(text_lines))
       last_time = end_time
       generate_svg(svg_canvas, text_lines)
--- gstreamer_tflite/detect.py
+++ gstreamer_tflite/detect.py
@@ -12,27 +12,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""A demo which runs object detection on camera frames.
-
-export TEST_DATA=/usr/lib/python3/dist-packages/edgetpu/test_data
-
+"""A demo which runs object detection on camera frames using GStreamer.
+TEST_DATA=../all_models
 Run face detection model:
-python3 -m edgetpuvision.detect \
+python3 detect.py \
   --model ${TEST_DATA}/mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite
-
 Run coco model:
-python3 -m edgetpuvision.detect \
+python3 detect.py \
   --model ${TEST_DATA}/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite \
   --labels ${TEST_DATA}/coco_labels.txt
 """
 import argparse
-import time
+import collections
+import imp
+import gstreamer
+import numpy as np
+import os
+from PIL import Image
 import re
 import svgwrite
-import imp
-import os
-from edgetpu.detection.engine import DetectionEngine
-import gstreamer
+import tflite_runtime.interpreter as tflite
+import time
+
+EDGETPU_SHARED_LIB = 'libedgetpu.so.1'
+Object = collections.namedtuple('Object', ['id', 'score', 'bbox'])
 
 def load_labels(path):
     p = re.compile(r'\s*(\d+)(.+)')
@@ -49,14 +52,75 @@ def generate_svg(dwg, objs, labels, text_lines):
     for y, line in enumerate(text_lines):
         shadow_text(dwg, 10, y*20, line)
     for obj in objs:
-        x0, y0, x1, y1 = obj.bounding_box.flatten().tolist()
+        x0, y0, x1, y1 = list(obj.bbox)
+        # Relative coordinates.
         x, y, w, h = x0, y0, x1 - x0, y1 - y0
+        # Absolute coordinates, input tensor space.
         x, y, w, h = int(x * width), int(y * height), int(w * width), int(h * height)
         percent = int(100 * obj.score)
-        label = '%d%% %s' % (percent, labels[obj.label_id])
+        label = '%d%% %s' % (percent, labels.get(obj.id, obj.id))
         shadow_text(dwg, x, y - 5, label)
         dwg.add(dwg.rect(insert=(x,y), size=(w, h),
-                        fill='red', fill_opacity=0.3, stroke='white'))
+                        fill='none', stroke='red', stroke_width='2'))
+
+def make_interpreter(model_file):
+    model_file, *device = model_file.split('@')
+    return tflite.Interpreter(
+      model_path=model_file,
+      experimental_delegates=[
+          tflite.load_delegate(EDGETPU_SHARED_LIB,
+                               {'device': device[0]} if device else {})
+      ])
+
+def input_size(interpreter):
+    """Returns input image size as (width, height) tuple."""
+    _, height, width, _ = interpreter.get_input_details()[0]['shape']
+    return width, height
+
+def input_tensor(interpreter):
+    """Returns input tensor view as numpy array of shape (height, width, 3)."""
+    tensor_index = interpreter.get_input_details()[0]['index']
+    return interpreter.tensor(tensor_index)()[0]
+
+def output_tensor(interpreter, i):
+    """Returns output tensor view."""
+    tensor = interpreter.tensor(interpreter.get_output_details()[i]['index'])()
+    return np.squeeze(tensor)
+
+def set_input(interpreter, data):
+    """Copies data to input tensor."""
+    input_tensor(interpreter)[:, :] = data
+
+def set_interpreter(interpreter, image, resample=Image.NEAREST):
+    image = image.resize((input_size(interpreter)), resample)
+    set_input(interpreter, image)
+    interpreter.invoke()
+
+class BBox(collections.namedtuple('BBox', ['xmin', 'ymin', 'xmax', 'ymax'])):
+    """Bounding box.
+    Represents a rectangle which sides are either vertical or horizontal, parallel
+    to the x or y axis.
+    """
+    __slots__ = ()
+
+def get_output(interpreter, score_threshold, image_scale=1.0):
+    """Returns list of detected objects."""
+    boxes = output_tensor(interpreter, 0)
+    class_ids = output_tensor(interpreter, 1)
+    scores = output_tensor(interpreter, 2)
+    count = int(output_tensor(interpreter, 3))
+
+    def make(i):
+        ymin, xmin, ymax, xmax = boxes[i]
+        return Object(
+            id=int(class_ids[i]),
+            score=scores[i],
+            bbox=BBox(xmin=np.maximum(0.0, xmin),
+                      ymin=np.maximum(0.0, ymin),
+                      xmax=np.minimum(1.0, xmax),
+                      ymax=np.minimum(1.0, ymax)))
+
+    return [make(i) for i in range(count) if scores[i] >= score_threshold]
 
 def main():
     default_model_dir = '../all_models'
@@ -74,18 +138,21 @@ def main():
     args = parser.parse_args()
 
     print("Loading %s with %s labels."%(args.model, args.labels))
-    engine = DetectionEngine(args.model)
+    interpreter = make_interpreter(args.model)
+    interpreter.allocate_tensors()
     labels = load_labels(args.labels)
 
     last_time = time.monotonic()
     def user_callback(image, svg_canvas):
       nonlocal last_time
       start_time = time.monotonic()
-      objs = engine.DetectWithImage(image, threshold=args.threshold,
-                                    keep_aspect_ratio=True, relative_coord=True,
-                                    top_k=args.top_k)
+
+      set_interpreter(interpreter, image)
+
+      objs = get_output(interpreter, args.threshold)
       end_time = time.monotonic()
       text_lines = [
+          ' ',
           'Inference: %.2f ms' %((end_time - start_time) * 1000),
           'FPS: %.2f fps' %(1.0/(end_time - last_time)),
       ]
