59fad4bd4e3ac8fa0bbdeb0a83f15add551b0776
manderfeld <manderfeld@google.com>
2019-12-02 11:29:17 -0800

    Detection with TFLite API works!

--- gstreamer/detect.py
+++ gstreamer/detect.py
@@ -12,23 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""A demo which runs object detection on camera frames.
-export TEST_DATA=/usr/lib/python3/dist-packages/edgetpu/test_data
+"""A demo which runs object detection on camera frames using GStreamer.
+TEST_DATA=../all_models
 Run face detection model:
-python3 -m edgetpuvision.detect \
+python3 detect.py \
   --model ${TEST_DATA}/mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite
 Run coco model:
-python3 -m edgetpuvision.detect \
+python3 detect.py \
   --model ${TEST_DATA}/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite \
   --labels ${TEST_DATA}/coco_labels.txt
 """
 import argparse
 import collections
-import imp
+import common
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst
 import gstreamer
 import numpy as np
 import os
-from PIL import Image
 import re
 import svgwrite
 import tflite_runtime.interpreter as tflite
@@ -47,19 +49,31 @@ def shadow_text(dwg, x, y, text, font_size=20):
     dwg.add(dwg.text(text, insert=(x+1, y+1), fill='black', font_size=font_size))
     dwg.add(dwg.text(text, insert=(x, y), fill='white', font_size=font_size))
 
-def generate_svg(dwg, objs, labels, text_lines):
-    width, height = dwg.attribs['width'], dwg.attribs['height']
-    for y, line in enumerate(text_lines):
+def generate_svg(src_size, inference_size, inference_box, objs, labels, text_lines):
+    dwg = svgwrite.Drawing('', size=src_size)
+    src_w, src_h = src_size
+    inf_w, inf_h = inference_size
+    box_x, box_y, box_w, box_h = inference_box
+    scale_x, scale_y = src_w / box_w, src_h / box_h
+
+    for y, line in enumerate(text_lines, start=1):
         shadow_text(dwg, 10, y*20, line)
     for obj in objs:
         x0, y0, x1, y1 = list(obj.bbox)
+        # Relative coordinates.
         x, y, w, h = x0, y0, x1 - x0, y1 - y0
-        x, y, w, h = int(x * width), int(y * height), int(w * width), int(h * height)
+        # Absolute coordinates, input tensor space.
+        x, y, w, h = int(x * inf_w), int(y * inf_h), int(w * inf_w), int(h * inf_h)
+        # Subtract boxing offset.
+        x, y = x - box_x, y - box_y
+        # Scale to source coordinate space.
+        x, y, w, h = x * scale_x, y * scale_y, w * scale_x, h * scale_y
         percent = int(100 * obj.score)
         label = '%d%% %s' % (percent, labels.get(obj.id, obj.id))
         shadow_text(dwg, x, y - 5, label)
         dwg.add(dwg.rect(insert=(x,y), size=(w, h),
-                        fill='red', fill_opacity=0.3, stroke='white'))
+                        fill='none', stroke='red', stroke_width='2'))
+    return dwg.tostring()
 
 def make_interpreter(model_file):
     model_file, *device = model_file.split('@')
@@ -71,12 +85,12 @@ def make_interpreter(model_file):
       ])
 
 def input_size(interpreter):
-    """Returns input image size as (width, height) tuple."""
-    _, height, width, _ = interpreter.get_input_details()[0]['shape']
-    return width, height
+    """Returns input size as (width, height, channels) tuple."""
+    _, height, width, channels = interpreter.get_input_details()[0]['shape']
+    return width, height, channels
 
 def input_tensor(interpreter):
-    """Returns input tensor view as numpy array of shape (height, width, 3)."""
+    """Returns input tensor view as numpy array of shape (height, width, channels)."""
     tensor_index = interpreter.get_input_details()[0]['index']
     return interpreter.tensor(tensor_index)()[0]
 
@@ -85,13 +99,16 @@ def output_tensor(interpreter, i):
     tensor = interpreter.tensor(interpreter.get_output_details()[i]['index'])()
     return np.squeeze(tensor)
 
-def set_input(interpreter, data):
+def set_input(interpreter, buf):
     """Copies data to input tensor."""
-    input_tensor(interpreter)[:, :] = data
+    result, mapinfo = buf.map(Gst.MapFlags.READ)
+    if result:
+        np_buffer = np.reshape(np.frombuffer(mapinfo.data, dtype=np.uint8), input_size(interpreter))
+        input_tensor(interpreter)[:, :] = np_buffer
+        buf.unmap(mapinfo)
 
-def set_interpreter(interpreter, image, resample=Image.NEAREST):
-    image = image.resize((input_size(interpreter)), resample)
-    set_input(interpreter, image)
+def set_interpreter(interpreter, data):
+    set_input(interpreter, data)
     interpreter.invoke()
 
 class BBox(collections.namedtuple('BBox', ['xmin', 'ymin', 'xmax', 'ymax'])):
@@ -101,12 +118,11 @@ class BBox(collections.namedtuple('BBox', ['xmin', 'ymin', 'xmax', 'ymax'])):
     """
     __slots__ = ()
 
-def get_output(interpreter, score_threshold, image_scale=1.0):
+def get_output(interpreter, score_threshold, top_k, image_scale=1.0):
     """Returns list of detected objects."""
     boxes = output_tensor(interpreter, 0)
     class_ids = output_tensor(interpreter, 1)
     scores = output_tensor(interpreter, 2)
-    count = int(output_tensor(interpreter, 3))
 
     def make(i):
         ymin, xmin, ymax, xmax = boxes[i]
@@ -117,8 +133,7 @@ def get_output(interpreter, score_threshold, image_scale=1.0):
                       ymin=np.maximum(0.0, ymin),
                       xmax=np.minimum(1.0, xmax),
                       ymax=np.minimum(1.0, ymax)))
-
-    return [make(i) for i in range(count) if scores[i] >= score_threshold]
+    return [make(i) for i in range(top_k) if scores[i] >= score_threshold]
 
 def main():
     default_model_dir = '../all_models'
@@ -136,30 +151,29 @@ def main():
     args = parser.parse_args()
 
     print("Loading %s with %s labels."%(args.model, args.labels))
-
     interpreter = make_interpreter(args.model)
     interpreter.allocate_tensors()
     labels = load_labels(args.labels)
 
-    last_time = time.monotonic()
-    def user_callback(image, svg_canvas):
-      nonlocal last_time
+    w, h, _ = input_size(interpreter)
+    inference_size = (w, h)
+    # Average fps over last 30 frames.
+    fps_counter  = common.avg_fps_counter(30)
+
+    def user_callback(input_tensor, src_size, inference_box):
+      nonlocal fps_counter
       start_time = time.monotonic()
-
-      set_interpreter(interpreter, image)
-
-      objs = get_output(interpreter, args.threshold)
+      set_interpreter(interpreter, input_tensor)
+      objs = get_output(interpreter, args.threshold, args.top_k)
       end_time = time.monotonic()
       text_lines = [
-          ' ',
           'Inference: %.2f ms' %((end_time - start_time) * 1000),
-          'FPS: %.2f fps' %(1.0/(end_time - last_time)),
+          'FPS: %d fps' % (round(next(fps_counter))),
       ]
       print(' '.join(text_lines))
-      last_time = end_time
-      generate_svg(svg_canvas, objs, labels, text_lines)
+      return generate_svg(src_size, inference_size, inference_box, objs, labels, text_lines)
 
-    result = gstreamer.run_pipeline(user_callback)
+    result = gstreamer.run_pipeline(user_callback, appsink_size=inference_size)
 
 if __name__ == '__main__':
     main()
