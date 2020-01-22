8142340b1743269eb5bc6b01a1caf3fe06d77eb6
Mike Tyka <mtyka@google.com>
2020-01-21 21:25:57 -0800

    Fix install_requirements.sh in opencv to work
    on mendel, raspbian and generic debian

--- opencv/install_requirements.sh
+++ opencv/install_requirements.sh
@@ -21,14 +21,19 @@ if grep -s -q "MX8MQ" /sys/firmware/devicetree/base/model; then
     echo "You must upgrade to Mendel 4.0 or higher."
     exit 1
   fi
+  sudo apt install python3-opencv
+elif grep -s -q "Raspberry Pi" /sys/firmware/devicetree/base/model; then
+  RASPBIAN=$(grep VERSION_ID /etc/os-release | sed 's/VERSION_ID="\([0-9]\+\)"/\1/')
+  echo "Raspbian Version: $RASPBIAN"
+  if [[ "$RASPBIAN" -ge "10" ]]; then
+    # Lock to version due to bug: https://github.com/piwheels/packages/issues/59
+    sudo pip3 install opencv-contrib-python==4.1.0.25
+    sudo apt-get -y install libjasper1 libhdf5-1* libqtgui4 libatlas-base-dev libqt4-test
+  else
+    echo "For Raspbian versions older than Buster (10) you have to build OpenCV yourself"
+    echo "or install the unofficial opencv-contrib-python package."
+    exit 1
+  fi
+else
+  sudo apt install python3-opencv
 fi
-
-# For Raspbian Buster and onwards the following works. For older versions
-# you can build OpenCV yourself or install the unofficial opencv-contrib-python package.
-
-# Lock to this version due to bug: https://github.com/piwheels/packages/issues/59
-sudo pip3 install opencv-contrib-python==4.1.0.25
-
-# Dependencies
-sudo apt-get -y install libjasper1 libhdf5-1* libqtgui4 libatlas-base-dev libqt4-test
-sudo apt install python3-opencv
